ce9feae673134b987975f05e82c3a87c93cc3a00
murat <muratdemir.pau@gmail.com>
2020-02-19 01:04:57 +0300

    190220

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace Infrastructure.Security
 {
-    public class UserAccessor: IUserAccessor
+    public class UserAccessor : IUserAccessor
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetCurrentUserName()
+        {
+            var userName = _httpContextAccessor.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            return userName;
+        }
     }
 }
