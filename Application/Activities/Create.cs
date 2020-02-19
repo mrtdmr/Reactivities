f0d6ef6a6261113561c8f4201f7a2721a3088340
mrtdmr <muratdemir.pau@gmail.com>
2020-02-19 16:24:13 +0300

    19022020

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,6 +1,8 @@
-﻿using Domain;
+﻿using Application.Interfaces;
+using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
 using System.Threading;
@@ -20,7 +22,8 @@ namespace Application.Activities
             public string City { get; set; }
             public string Venue { get; set; }
         }
-        public class CommandValidator : AbstractValidator<Command> {
+        public class CommandValidator : AbstractValidator<Command>
+        {
             public CommandValidator()
             {
                 RuleFor(x => x.Title).NotEmpty();
@@ -34,9 +37,12 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
@@ -51,7 +57,16 @@ namespace Application.Activities
                     Venue = request.Venue
                 };
                 _context.Activities.Add(activity);
-                var success = await _context.SaveChangesAsync()>0;
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+                _context.UserActivities.Add(attendee);
+                var success = await _context.SaveChangesAsync() > 0;
                 if (success)
                     return Unit.Value;
                 throw new Exception("Problem saving changes.");
