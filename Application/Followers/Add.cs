bd62aa4491df8e83b503a2a007005f02fa7891e6
mrtdmr <muratdemir.pau@gmail.com>
2020-03-02 16:16:57 +0300

    02032020

---
+++ Application/Followers/Add.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Followers
+{
+    public class Add
+    {
+        public class Command : IRequest {
+            public string UserName { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context,IUserAccessor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var observer = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
+                var target = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.UserName);
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Persistence/Migrations/20200302112706_Added following entitiy.Designer.cs
+++ Persistence/Migrations/20200302112706_Added following entitiy.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200302112706_Added following entitiy")]
+    partial class Addedfollowingentitiy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
